4a23c124d1d7c92bf15fc022d5764ff267dde5ea
Bchain91 <baecain@gmail.com>
2023-05-08 19:47:44 +0900

    opencv

---
+++ OpenCVAppGUI/resource.h
@@ -0,0 +1,30 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// OpenCVAppGUI.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_OPENCVAPPGUI_DIALOG         102
+#define IDR_MAINFRAME                   128
+#define IDC_BTN_LOAD                    1042
+#define IDC_BTN_SAVE                    1043
+#define IDC_BTN_INSPECTION              1044
+#define IDC_BTN_INSPECTION_CV           1045
+#define IDC_PICTURE                     1046
+#define IDC_PICTURE_DEBUG               1047
+#define IDC_PICTURE_RESULT              1048
+#define IDC_BTN_LOAD2                   1049
+#define IDC_BTN_SAMPLE_CODE             1051
+#define IDC_LIST_LOG                    1053
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1054
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
